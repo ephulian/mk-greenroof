154a17f2a8efb4494207825978565bc6cf4f66e6
kirilkomitski <k.komitski94@gmail.com>
2021-09-19 14:48:55 +0100

    Edited converter to match new data format Typo corrected

--- roofsensors/converter.py
+++ roofsensors/converter.py
@@ -11,12 +11,11 @@
 # Next 17 bits are Illumination = 000000000111001001 =
 # Illumination Range is 0 to 100,000 for 0 to 100,000 so 225 = 225 lx (edited)
 
-class SensorDataConverter:
+class SensorDataConverter():
 
-
-    def __init__(self, incoming_data):
+    def __init__(self, device_id, sensor_data):
         self.incoming_data = incoming_data
-        sensor_data = incoming_data[-18:]
+        #sensor_data = incoming_data[-18:]
         scale = 16
         num_of_bits = 8
         input_data = bin(int(sensor_data, scale))[2:].zfill(num_of_bits)
@@ -25,12 +24,13 @@ class SensorDataConverter:
         self.hum_input = input_data[10:18]
         self.illum_input = input_data[18:35]
 
-        self.year = incoming_data[:4]
-        self.month = incoming_data[5:7]
-        self.date = incoming_data[8:10]
-        self.time = incoming_data[11:19]
+        # self.year = incoming_data[:4]
+        # self.month = incoming_data[5:7]
+        # self.date = incoming_data[8:10]
+        #
+        # self.time = incoming_data[11:19]
 
-        self.device_id = incoming_data[24:32]
+        self.device_id = device_id
 
 
     def temp_range(self):
@@ -47,11 +47,11 @@ class SensorDataConverter:
         new_max = 100000
         new_min = 0
         old_range = (old_max - old_min)
-        if oldrange == 0:
+        if old_range == 0:
             output = new_min
         else:
             new_range = (new_max - new_min)
-            output = (((int(self.illum_input, 2) - old_min) * new_range) / old_range) + new_min
+            output = (((int(self.illum_input, 2) - old_min) * new_range) / oldrange) + new_min
         return output
 
 # Returns battery charge if available, returns False otherwise
