0c3b01e857f8e2145e50a05d4bb0636985d09ff8
Steven Waterfall <stevenwaterfall@gmail.com>
2021-09-19 15:18:44 +0100

    Added models to admin

--- roofsensors/admin.py
+++ roofsensors/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import ExperimentData, AmbientData
 
-# Register your models here.
+class AmbientAdmin(admin.ModelAdmin):
+    list_display = (
+        'recorded_at',
+        'red_one_temperature',
+        'red_one_humidity',
+        'red_one_illumination',
+        'red_two_temperature',
+        'red_two_humidity',
+        'red_two_illumination',
+        'red_three_temperature',
+        'red_three_humidity',
+        'red_three_illumination')
+    model = AmbientData
+
+class ExperimentAdmin(admin.ModelAdmin):
+    list_display = (
+        'recieved_at',
+        'green_roof_top',
+        'green_roof_middle',
+        'green_roof_bottom',
+        'control_top',
+        'control_bottom',
+        'moist_s0',
+        'moist_s1',)
+    model = ExperimentData
+
+admin.site.register(ExperimentData, ExperimentAdmin)
+admin.site.register(AmbientData, AmbientAdmin)
