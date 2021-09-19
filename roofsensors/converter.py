aa720086f32b8dccf1307901f10c931b3aafc291
kirilkomitski <k.komitski94@gmail.com>
2021-09-19 15:53:44 +0100

    Tidied up

--- roofsensors/converter.py
+++ roofsensors/converter.py
@@ -11,11 +11,9 @@
 # Next 17 bits are Illumination = 000000000111001001 =
 # Illumination Range is 0 to 100,000 for 0 to 100,000 so 225 = 225 lx (edited)
 
-class SensorDataConverter():
+class SensorDataConverter:
 
     def __init__(self, device_id, sensor_data):
-        # self.incoming_data = incoming_data
-        # sensor_data = incoming_data[-18:]
         scale = 16
         num_of_bits = 8
         input_data = bin(int(sensor_data, scale))[2:].zfill(num_of_bits)
@@ -24,25 +22,19 @@ class SensorDataConverter():
         self.hum_input = input_data[10:18]
         self.illum_input = input_data[18:35]
 
-        # self.year = incoming_data[:4]
-        # self.month = incoming_data[5:7]
-        # self.date = incoming_data[8:10]
-        #
-        # self.time = incoming_data[11:19]
-
         self.device_id = device_id
 
-
+    # Returns the temperature in C
     def temp_range(self):
         output = (int(self.temp_input, 2) / 10) - 40
         return round(output, 2)
 
-
+    # Returns humidity in %
     def hum_range(self):
         output = int(self.hum_input, 2)/2
         return round(output, 2)
 
-
+    #Returns a rounded decimal of lx
     def illum_range(self):
         old_max = 255
         old_min = 0
@@ -56,7 +48,7 @@ class SensorDataConverter():
             output = (((int(self.illum_input, 2) - old_min) * new_range) / old_range) + new_min
         return round(output, 2)
 
-
+    # Checks device ID and returns the name and location
     def sensor_location(self):
         device_location = ''
         device_name = ''
@@ -78,17 +70,3 @@ def battery_reading(incoming_data):
         battery_percent = incoming_data[49:53]
         return battery_percent
     return False
-# Checks the device ID and returns its name and location
-# def sensor_location(device_id):
-#     device_location = ''
-#     device_name = ''
-#     if device_id == '0420F51E':
-#         device_location = 'Near Green Roof'
-#         device_name = 'Red 1'
-#     elif device_id == '0420 FDDA':
-#         device_location = 'Near Control Slab'
-#         device_name = 'Red 2'
-#     elif device_id == '0420 F51F':
-#         device_location = 'Enviroment Temp'
-#         device_name = 'Red 3'
-#     return device_location, device_name
